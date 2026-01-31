76a002fec522bd7a2dbaba07a0c320dc4d9a9d47
Simran Kumari <kumarisimran1504@gmail.com>
2026-01-31 21:12:46 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,24 @@
+#Simple command to add
+a = 1
+b = 2
+print(a + b)        #This will give the output 3
+
+#Subtract
+a = 1
+b = 2
+print(b - a)        #This will give the output 1
+
+#Multiply
+a = 2
+b = 8
+print(b * a)        #This will give the output 16
+
+#Divide
+a = 2
+b = 16
+print(b / a)        #This will give the output 8.0. As we have used a single slash(/) to divide.
+
+#Divide
+a = 2
+b = 16
+print(b // a)       #This will give the output 8. As we have used double slash(//) to divide.
