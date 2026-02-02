ff4525ab285cba379a2ac42faabddf458a0f5173
Simran Kumari <kumarisimran1504@gmail.com>
2026-02-02 21:10:45 +0530

    Add files via upload

---
+++ Simple_Interest.py
@@ -0,0 +1,8 @@
+print("Enter the Principal: ")
+p = int(input())
+print("Enter the Rate of Interest: ")
+r = int(input())
+print("Enter the Time(in years): ")
+t = int(input())
+SI = (p*r*t)/100
+print(f"The Simple Interest is: {SI}")
