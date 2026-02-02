ba3d48c00aef671546335b7badd201a577d20e35
Simran Kumari <kumarisimran1504@gmail.com>
2026-02-02 21:55:18 +0530

    Simple_Interest.py

--- Simple_Interest.py
+++ Simple_Interest.py
@@ -1,8 +1,10 @@
+#Q. How to calculate the Simple Interest?
 print("Enter the Principal: ")
 p = int(input())
 print("Enter the Rate of Interest: ")
 r = int(input())
 print("Enter the Time(in years): ")
 t = int(input())
-SI = (p*r*t)/100
+SI = (p*r*t)/100            #Formula for the Simple Interest
+
 print(f"The Simple Interest is: {SI}")
