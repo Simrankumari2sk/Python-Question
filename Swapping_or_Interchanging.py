1fabd1bb93f7b1ef15390ad1eb80947f134e6ecd
Simran Kumari <kumarisimran1504@gmail.com>
2026-02-04 21:25:34 +0530

    Add files via upload

---
+++ Swapping_or_Interchanging.py
@@ -0,0 +1,10 @@
+#Q. Write a program for Swapping or Interchanging two values.
+print("Enter the First value: ")
+A = input()
+print("Enter the Second value: ")
+B = input()
+temp = A
+A = B
+B = temp
+print("The First value becomes", A)
+print("The Second value becomes", B)
