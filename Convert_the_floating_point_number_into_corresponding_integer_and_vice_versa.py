220865ff73f3ef39e280189c33e3d32eb9e70203
Simran Kumari <kumarisimran1504@gmail.com>
2026-02-19 21:06:28 +0530

    Add files via upload

---
+++ Convert_the_floating_point_number_into_corresponding_integer_and_vice_versa.py
@@ -0,0 +1,7 @@
+#Q. Write a program to convert the floating point number into corresponding integer.
+float_number = float(input("Enter the floating number: "))
+print(f"The interger variant of {str(float_number)} is {str(int(float_number))}")
+
+#Q. Write a program to convert an integer into floating point number.
+integer_number = int(input("Enter an integer: "))
+print(f"The floating variant of {str(integer_number)} is {str(float(integer_number))}")
